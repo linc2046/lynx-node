d43e4cab3a3cc3941d69886315f78dcc511dc0e6
linc2046 <linc2046@gmail.com>
2022-09-20 21:20:09 +0800

    test node

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use neon::prelude::*;
+use lynxlang::env::Env;
+use lynxlang::evaluator::Evaluator;
+use lynxlang::parser::Parser;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn run(mut cx: FunctionContext) -> JsResult<JsString> {
+    // https://neon-bindings.com/docs/functions
+    let handles = cx.argument::<JsString>(0)?;
+    let input = format!("{:?}", handles.upcast::<JsValue>());
+
+    println!("{:?}", input);
+
+    let program = Parser::get(&input.as_str()).parse_program();
+    let mut evaluator = Evaluator::new(Rc::new(RefCell::new(Env::new())));
+    evaluator.builtin();
+
+    match evaluator.eval_program(program) {
+        Some(value) => Ok(cx.string(format!("{:?}", value))),
+        None => Ok(cx.string("Null")),
+    }    
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("run", run)?;
+    Ok(())
+}
